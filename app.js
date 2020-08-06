4f4d3c9c51e8705417a0773bb9e0bad9cd229858
Sunny Singh Bhullar <sunny.singh@bellmedia.ca>
2020-08-06 13:08:17 -0400

    This is intended to be the fizx for proxy

--- app.js
+++ app.js
@@ -3,11 +3,22 @@ var bodyParser = require("body-parser");
 var routes = require("./routes/routes.js");
 var app = express();
 const fs = require('fs');
+var https = require('https');
 
 const options = {
   key: fs.readFileSync('key.pem'),
   cert: fs.readFileSync('cert.pem')
 };
+
+const proxy = require("node-global-proxy").default;
+
+proxy.setConfig({
+  http: "http://fastweb.int.bell.ca:8083",
+  //https: "https://fastweb.int.bell.ca:8083",
+});
+proxy.start();
+
+
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
